7b64195b84fef7eda830d7affada877649b0a52c
AngelFBI <angel.bejarano@enroutesystems.com>
2022-12-06 14:08:09 -0600

    Added validations and products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,36 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_product
+    begin
+      @product = Product.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+    end
+
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :image_url)
+  end
 end
