559b4d8cb11081ce1b56ec42079ab4dae44d7e04
AngelFBI <angel.bejarano@enroutesystems.com>
2022-12-06 14:50:43 -0600

    Added validations to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  before_save { self.email.downcase! }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :first_name, :last_name, :email, presence: true
+  validates :email,
+    uniqueness: { case_sensitive: false },
+    format: { with: VALID_EMAIL_REGEX}
+  has_secure_password
 end
