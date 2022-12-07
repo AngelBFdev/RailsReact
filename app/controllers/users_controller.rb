1337d10055e4b135d39e9ac0e6b3d6b31e6455c5
AngelFBI <angel.bejarano@enroutesystems.com>
2022-12-07 12:42:04 -0600

    Created sessions and users controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+end
