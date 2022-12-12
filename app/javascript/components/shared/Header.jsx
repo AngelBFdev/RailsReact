ad04d0963e92ec33bcdc933347d3cabc0cc2cc2c
AngelFBI <angel.bejarano@enroutesystems.com>
2022-12-12 12:29:50 -0600

    Fixed Jumbotron and ProductsList

--- app/javascript/components/shared/Header.jsx
+++ app/javascript/components/shared/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => (
     </button>
 
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ml-auto">
+      <ul className="navbar-nav ms-auto">
         <li className="nav-item">
           <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
         </li>
