1425357551c514038d0f65d879f348c20a737b6d
AngelFBI <angel.bejarano@enroutesystems.com>
2022-12-12 18:17:54 -0600

    Added axios

--- app/javascript/containers/ProductsContainer.jsx
+++ app/javascript/containers/ProductsContainer.jsx
@@ -1,24 +1,52 @@
 import React from 'react'
+import axios from 'axios'
 
 import Product from '../components/products/Product'
 
-const ProductList = () => {
-  const products = ['Product1', 'Product2', 'Product3']
-  const productList = products.map(product => <Product key={product}/>)
-  return(
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12 mb-2">
-          <div className="row">
+class ProductList extends React.Component {
+  constructor(props){
+    super(props)
 
-          { productList }
+    this.state = {
+      products: []
+    }
+  }
 
+  componentDidMount(){
+    this.loadProductsFromServer()
+  }
+
+  loadProductsFromServer() {
+    axios
+      .get('/api/v1/products.json')
+      .then(response => {
+        const { products } = response.data
+        this.setState({ products })
+      })
+      .catch(error => console.log(error.response.data))
+
+  }
+
+  render () {
+    const products = ['Product1', 'Product2', 'Product3']
+    const productList = products.map(
+      product => <Product key={product}/>
+    )
+
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 mb-2">
+            <div className="row">
+
+            { productList }
+
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
-
+    )
+  }
 }
 
 export default ProductList
