36d4f1c2a2eee8197577aeab77a7ae09664407a5
AngelFBI <angel.bejarano@enroutesystems.com>
2022-12-14 12:51:39 -0600

    Created Product Detail
    - Fixed links in Header and App

--- app/javascript/components/shared/Header.jsx
+++ app/javascript/components/shared/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => (
   <nav className="navbar navbar-expand-lg navbar-light ">
-    <a className="navbar-brand goog" href="index.html">O-Sale</a>
+    <Link className='navbar-brand goog' to='/'>O-Sale</Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -10,7 +11,7 @@ const Header = () => (
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto">
         <li className="nav-item">
-          <a className="nav-link" href="index.html">Home </a>
+          <Link className='nav-link' to='/'>Home</Link>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="new-item.html">New Product</a>
--- app/javascript/containers/App.jsx
+++ app/javascript/containers/App.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Header from '../components/shared/Header'
-import Jumbotron from '../components/products/Jumbotron'
 import ProductList from './ProductsContainer'
 import Footer from '../components/shared/Footer'
+import ProductDetail from './ProductDetailContainer'
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <Routes>
       <Route exact path='/' element={<ProductList/>}/>
+      <Route path='/product/:id' element={<ProductDetail/>}/>
     </Routes>
     <Footer />
   </BrowserRouter>
