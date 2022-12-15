0193fe543055821ca008a0afbbd8af411afc3720
AngelFBI <angel.bejarano@enroutesystems.com>
2022-12-15 11:27:10 -0600

    Created NewProductForm component
    - Changed App functional component into a class component
    - Changed methods into arrow functions in ProductsContainer

--- app/javascript/containers/App.jsx
+++ app/javascript/containers/App.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Header from '../components/shared/Header'
 import ProductList from './ProductsContainer'
 import Footer from '../components/shared/Footer'
 import ProductDetail from './ProductDetailContainer'
+import NewProductForm from '../components/products/NewProductForm'
 
-const App = () => (
-  <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route exact path='/' element={<ProductList/>}/>
-      <Route path='/product/:id' element={<ProductDetail/>}/>
-    </Routes>
-    <Footer />
-  </BrowserRouter>
-)
+class App extends Component {
+  render(){
+    return(
+       <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<ProductList/>}/>
+          <Route path='/product/:id' element={<ProductDetail/>}/>
+          <Route path='/newProduct' element={<NewProductForm/>}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    )
+  }
+
+}
 
 export default App
