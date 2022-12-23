9f2ab653023391b788b08dacf569e61caca69fdb
AngelFBI <angel.bejarano@enroutesystems.com>
2022-12-23 11:47:40 -0600

    Fixed bug of displaying server errors

--- app/javascript/components/products/NewProductForm.jsx
+++ app/javascript/components/products/NewProductForm.jsx
@@ -28,15 +28,28 @@ class NewProductForm extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
-    const { name, description, price, quantity } = this.state;
+    const fieldNames = ["name", "description", "price", "quantity"];
+    this.verifyAndSetFieldErrors(fieldNames);
 
-    const newProduct = {
-      name,
-      description,
-      price,
-      quantity,
-    };
-    this.props.onSubmit(newProduct);
+    if (Object.keys(this.state.errors).length === 0) {
+      const { name, description, price, quantity } = this.state;
+
+      const newProduct = { name, description, price, quantity };
+      this.props.onSubmit(newProduct);
+    }
+  };
+
+  verifyAndSetFieldErrors = (fieldNames) => {
+    let errors = {};
+
+    fieldNames.forEach((fieldName) => {
+      const fieldError = this.checkErrors(this.state, fieldName);
+      errors = Object.assign({}, errors, fieldError); // errors = { ...errors, ...fieldError}
+    });
+
+    if (Object.keys(errors).length > 0) {
+      this.setState({ errors });
+    }
   };
 
   handleChange = (event) => {
