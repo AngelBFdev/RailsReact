0b11dda7be40488caa2628daa0a1cff69ed2e940
AngelFBI <angel.bejarano@enroutesystems.com>
2022-12-24 21:54:00 -0600

    Fixed Header now hides unnecesary links

--- app/javascript/components/shared/Header.jsx
+++ app/javascript/components/shared/Header.jsx
@@ -2,58 +2,72 @@ import React from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Header = ({ currentUser, onSignout, location, history }) => (
-  <nav className="navbar navbar-expand-lg navbar-light ">
-    <Link className="navbar-brand goog" to="/">
-      O-Sale
-    </Link>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
+const Header = ({ currentUser, onSignout, location, history }) => {
+  const authLinks = currentUser && (
+    <ul className="navbar-nav ms-auto">
+      <li className="nav-item">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#"
+          onClick={(event) => onSignout(event, location, history)}
+          className="nav-link"
+        >
+          Sign Out
+        </a>
+      </li>
+      <li className="nav-item mt-1">
+        <p className="navbar-text"> Signed in as {currentUser.email}</p>
+      </li>
+    </ul>
+  );
 
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">
-            Sign In
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/register">
-            Sign Up
-          </Link>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#"
-            onClick={(event) => onSignout(event, location, history)}
-            className="nav-link"
-          >
-            Sign Out
-          </a>
-        </li>
-        {currentUser ? (
-          <li className="nav-item mt-1">
-            <p className="navbar-text"> Signed in as {currentUser.email}</p>
-          </li>
-        ) : null}
-      </ul>
-    </div>
-  </nav>
-);
+  const unAuthLinks = !currentUser && (
+    <ul className="navbar-nav ms-auto">
+      <li className="nav-item">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/login">
+          Sign In
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/register">
+          Sign Up
+        </Link>
+      </li>
+    </ul>
+  );
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light ">
+      <Link className="navbar-brand goog" to="/">
+        O-Sale
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        {currentUser ? authLinks : unAuthLinks}
+      </div>
+    </nav>
+  );
+};
 
 Header.propTypes = {
   currentUser: PropTypes.object,
